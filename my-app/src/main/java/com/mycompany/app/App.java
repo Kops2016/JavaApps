a3a325d33745fe6f3c8a8a3cfb3abe6b3576a233
Kops2016 <cruma2010@gmail.com>
2016-02-20 23:52:30 +0530

    hello world message changed

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -20,7 +20,7 @@ public class App extends AbstractHandler {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
+        response.getWriter().println("<h1>Hello World from V10</h1>");
     }
 
     public static void main(String[] args) throws Exception {
