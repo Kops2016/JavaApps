1928b7a5d66f5423f087c407d89704746b6d659e
Kops2016 <cruma2010@gmail.com>
2016-02-20 23:53:59 +0530

    hello world from Mars

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -21,6 +21,7 @@ public class App extends AbstractHandler {
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
         response.getWriter().println("<h1>Hello World from V10</h1>");
+        response.getWriter().println("<h1>Hello Mars from V10</h1>");
     }
 
     public static void main(String[] args) throws Exception {
